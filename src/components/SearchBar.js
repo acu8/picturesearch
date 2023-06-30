dea2fec6a998331436509fa6ba64783bfe5810b4
acu8 <ewatanabe786@gmail.com>
2023-06-30 22:06:54 +0900

    event handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
-export default function SearchBar() {
+export default function SearchBar({onSubmit}) {
+  const [term, setTerm] = useState();
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    onSubmit(event.target.value);
+  }
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
+  }
+
   return (
-    <div>SearchBar</div>
-  )
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <input onChange={handleChange} />
+      </form>
+    </div>
+  );
 }
