eec1e05d9831536a2795d4886ae9742d1b9fdb1a
acu8 <ewatanabe786@gmail.com>
2023-07-02 22:15:26 +0900

    images

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const searchImages = async() => {
+const searchImages = async(term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
-            Authorization: 'Client-ID PAt_izADRvVpzQx2pRmMYy6PjsMjqElzZfzIV1tIC98'
+            Authorization: 'Client-ID 58Vb-ZTjlVvv5x2KJ5WkcT4WfHfMnRbD0W22DZQsJ4k'
         },
         params: {
             query: term,
